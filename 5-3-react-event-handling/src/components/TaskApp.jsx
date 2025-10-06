f7f0b46f7ca081c39a8a932455cb620929482274
Abdulellah Alamer <122591461+AbdulellahAlamer@users.noreply.github.com>
2025-10-06 09:02:35 +0300

    task 1

--- 5-3-react-event-handling/src/components/TaskApp.jsx
+++ 5-3-react-event-handling/src/components/TaskApp.jsx
@@ -2,19 +2,20 @@ import React, { useState } from "react";
 import TaskList from "./TaskList";
 
 export default function TaskApp() {
-  
+  const [text, setText] = useState("");
+  // const [tasks, setTasks] = useState([]);
+
   const handleSubmit = () => {
-   
+    // setTasks((prev) => [...prev, { id: Date.now(), text }]);
+    // setText("");
   };
 
-  
   const handleDelete = (id) => {
-    // TODO: filter tasks by id to remove the clicked one
+    // setTasks((prev) => prev.filter((task) => task.id !== id));
   };
 
-  
   const handleClearAll = () => {
-    // TODO: set tasks to empty array
+    // setTasks([]);
   };
 
   return (
@@ -25,8 +26,8 @@ export default function TaskApp() {
           type="text"
           placeholder="Type a task..."
           className="input"
-          // TODO: value={text}
-          // TODO: onChange={(e) => setText(e.target.value)}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
           onKeyDown={(e) => {
             if (e.key === "Enter") handleSubmit();
           }}
@@ -35,10 +36,11 @@ export default function TaskApp() {
           Submit
         </button>
       </div>
+      <p>{text}</p>
 
       {/*Render Task List and Enable Delete */}
       {/*Pass tasks and onDelete */}
-      <TaskList /* tasks={tasks} onDelete={handleDelete} */ />
+      {/* <TaskList tasks={tasks} onDelete={handleDelete} /> */}
 
       {/*Clear All */}
       <div className="footerRow">
